076dd507cb4f05128c2155674ec36c40a58e40e2
LeanLabiano <leandrodenos@gmail.com>
2025-01-25 04:54:05 -0300

    fix text color

--- components/game/pointsInfo.tsx
+++ components/game/pointsInfo.tsx
@@ -16,10 +16,12 @@ const PointsInfo: React.FC<ModalProps> = ({
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-black/50 rounded-lg shadow-lg p-6 w-96 flex flex-col items-center border-2 border-[#ff8a00]">
-        <h2 className="text-4xl text-center text-white mb-4">{title}</h2>
-        <p>Total Bird Points: {points}</p>
-        <p className="text-gray-300 text-center ">{resultMessage}</p>
-        <p className="text-gray-300 text-center mb-6">Maybe nothing, but...</p>
+        <h2 className="text-4xl text-center font-bold text-[#ffe500] mb-4">
+          {title}
+        </h2>
+        <p className="text-white text-xl">Total Bird Points: {points}</p>
+        <p className="text-gray-200 text-center ">{resultMessage}</p>
+        <p className="text-gray-200 text-center mb-6">Maybe nothing, but...</p>
         <button
           onClick={onClose}
           className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
