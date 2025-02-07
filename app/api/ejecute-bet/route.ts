a0c942e9022b374a6279d45ee3a8563cd2c3a71a
LeanLabiano <leandrodenos@gmail.com>
2025-02-06 21:17:25 -0300

    1200.. gas

--- app/api/ejecute-bet/route.ts
+++ app/api/ejecute-bet/route.ts
@@ -38,7 +38,7 @@ export async function POST(_request: NextRequest) {
 
   try {
     const resultBet = await contract._settleBet(pendingId, randomNumber, {
-      gasPrice: 800000,
+      gasPrice: 1200000,
       gasLimit: 200000, // Keep your gas limit
     });
 
